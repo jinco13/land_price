5ac5cc14a2956b593f924d209189e8543388e380
jinco13 <jinco13@users.noreply.github.com>
2017-10-21 21:51:37 +0900

    added areas based on web api

--- tests/app/WebAPITest.php
+++ tests/app/WebAPITest.php
@@ -12,4 +12,15 @@ class WebAPITest extends \PHPUnit_Framework_TestCase
   {
     $this->assertInstanceOf('\LandPrice\WebAPI', $this->target);
   }
+
+  public function testClientInstance()
+  {
+    $this->assertInstanceOf('\GuzzleHttp\Client', $this->target->client);
+  }
+
+  public function testTokyoAreaCode()
+  {
+    $this->assertEquals($this->target->getAreaCode('東京都'), "13");
+    $this->assertEquals($this->target->getAreaCode('神奈川県'), "14");
+  }
 }
